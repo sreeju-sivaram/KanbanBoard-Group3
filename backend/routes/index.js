c9f7d4a16cfbf7113a90137729d73afd298b2126
AjayTumula <ajaytumula72@gmail.com>
2022-08-24 17:00:38 +0100

    Implemented login and register controller routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 const userRoutes = require('./users')
 const taskRoutes = require('./tasks')
 const statusesRoutes = require('./statuses')
+const login = require('./login')
+const register = require('./register')
 
 var router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/tasks", taskRoutes);
 router.use("/statuses", statusesRoutes);
+router.use("/login", login);
+router.use("/register", register);
 
 module.exports = router;
