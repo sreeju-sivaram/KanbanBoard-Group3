3255030244ffc4697e2c8c888850689bbc7fd342
AjayTumula <ajaytumula72@gmail.com>
2022-08-25 10:45:36 +0100

    added routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tasksRouter = require('./routes/tasks');
 var statusesRouter = require('./routes/statuses');
+var loginRouter = require('./routes/login');
+var registerRouter = require('/routes/register');
 
 var app = express();
 
@@ -26,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use('/statuses', statusesRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 app.use(cors({
   origin: '*'
